36509f9c93bf8ccc4c02a1b0b7ef7f6203e5b8e6
Alexander <alex_osilva@hotmail.com>
2021-07-20 02:39:53 -0300

    feat: add person updater

--- src/main/java/dev/xndr/peopleapi/service/PersonService.java
+++ src/main/java/dev/xndr/peopleapi/service/PersonService.java
@@ -3,12 +3,11 @@ package dev.xndr.peopleapi.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import dev.xndr.peopleapi.dto.request.PersonDTO;
 import dev.xndr.peopleapi.dto.response.MessageResponseDTO;
@@ -31,10 +30,7 @@ public class PersonService {
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -59,6 +55,22 @@ public class PersonService {
     public void deleteById(Long id) throws PersonNotFoundException {
         verifyIfExists(id);
         personRepository.deleteById(id);
+    }
+
+    public MessageResponseDTO updateById(Long id, @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+        
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 
 }
